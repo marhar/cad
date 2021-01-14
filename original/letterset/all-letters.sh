a329cc2fa01a990b1ad134528822023ddc65b813
Mark Harrison <marhar@gmail.com>
2021-01-13 16:44:19 -0800

    Renamed file.

--- original/letterset/all-letters.sh
+++ original/letterset/all-letters.sh
@@ -2,9 +2,9 @@
 OPENSCAD=/Applications/OpenSCAD-2019.05.app/Contents/MacOS/OpenSCAD
 for i in A B C D E F G H I J K L M N O P Q R S T U V W X Y Z; do
   echo generating $i
-  time $OPENSCAD  -D letter=\"$i\" -o upper_$i.stl one-letter.scad
+  time $OPENSCAD  -D letter=\"$i\" -o upper_$i.stl a-letter.scad
 done
 for i in a b c d e f g h i j k l m n o p q r s t u v w x y z; do
   echo generating $i
-  time $OPENSCAD  -D letter=\"$i\" -o lower_$i.stl one-letter.scad
+  time $OPENSCAD  -D letter=\"$i\" -o lower_$i.stl a-letter.scad
 done
